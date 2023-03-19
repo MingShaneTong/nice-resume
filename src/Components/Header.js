70b686d167e84756bf5f19d6443910dfd1b9d5c9
MingShaneTong <63452934+MingShaneTong@users.noreply.github.com>
2023-03-19 20:11:30 +1300

    Insert data (#1)
    * added data
    
    * scroll down without hash
    
    * button up

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,7 @@ class Header extends Component {
   render() {
     if (!this.props.data) return null;
 
-    const project = this.props.data.project;
+    const linkedin = this.props.data.linkedin;
     const github = this.props.data.github;
     const name = this.props.data.name;
     const description = this.props.data.description;
@@ -62,13 +62,13 @@ class Header extends Component {
               <h1 className="responsive-headline">{name}</h1>
             </Fade>
             <Fade bottom duration={1200}>
-              <h3>{description}.</h3>
+              <h3>{description}</h3>
             </Fade>
             <hr />
             <Fade bottom duration={2000}>
               <ul className="social">
-                <a href={project} className="button btn project-btn">
-                  <i className="fa fa-book"></i>Project
+                <a href={linkedin} className="button btn">
+                  <i className="fa fa-linkedin"></i>LinkedIn
                 </a>
                 <a href={github} className="button btn github-btn">
                   <i className="fa fa-github"></i>Github
@@ -79,8 +79,14 @@ class Header extends Component {
         </div>
 
         <p className="scrolldown">
-          <a className="smoothscroll" href="#about">
+          <a className="smoothscroll" data-anchor="#about">
             <i className="icon-down-circle"></i>
+          </a>
+        </p>
+
+        <p className="scrollup" id="scrollup-button">
+          <a className="smoothscroll" data-anchor="#home">
+            <i className="icon-up-circle"></i>
           </a>
         </p>
       </header>

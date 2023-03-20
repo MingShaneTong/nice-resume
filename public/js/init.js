256e951ac6f34f8497fbc024dcc47ae8e19643cb
Ming Shane Tong <mstong800@gmail.com>
2023-03-20 23:46:59 +1300

    include gh-pages

--- public/js/init.js
+++ public/js/init.js
@@ -1,13 +1,18 @@
+const resizeAction = function() {
+  $("header#home").css({ height: $(window).height() });
+  $("body").css({ width: $(window).width() });
+  if ($(window).width() >= 767) {
+    $("#nav-wrap > .mobile-btn").hide();
+    $("#nav-wrap > ul#nav").show();
+  } else {
+    $("#nav-wrap > .mobile-btn").show();
+  }
+}
+
 jQuery(document).ready(function($) {
   var time = 300;
   setTimeout(function() {
-    if ($(window).width() >= 767) {
-      $("#nav-wrap > .mobile-btn").hide();
-      $("#nav-wrap > ul#nav").show();
-    } else {
-      $("#nav-wrap > .mobile-btn").show();
-      $("#nav-wrap > ul#nav").hide();
-    }
+    resizeAction();
     
     $("h1.responsive-headline").fitText(1, { minFontSize: "40px", maxFontSize: "90px" });
 
@@ -28,16 +33,7 @@ jQuery(document).ready(function($) {
     });
 
     $("header#home").css({ height: $(window).height() });
-    $(window).on("resize", function() {
-      $("header#home").css({ height: $(window).height() });
-      $("body").css({ width: $(window).width() });
-      if ($(window).width() >= 767) {
-        $("#nav-wrap > .mobile-btn").hide();
-        $("#nav-wrap > ul#nav").show();
-      } else {
-        $("#nav-wrap > .mobile-btn").show();
-      }
-    });
+    $(window).on("resize", resizeAction);
 
     $(window).on("scroll", function() {
       var h = $("header#home").height();

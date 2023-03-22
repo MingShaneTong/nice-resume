61640acb4964d060d127c0f09af141b5823ee1b4
Ming Shane Tong <mstong800@gmail.com>
2023-03-22 22:54:46 +1300

    different colours for different projects

--- src/Components/Projects/Portfolio.js
+++ src/Components/Projects/Portfolio.js
@@ -8,31 +8,34 @@ class Portfolio extends Component {
     const projects = this.props.data.projects.map(function(project) {
       return (
         <Fade duration={1000}>
-          <div className="row project">
-            <div className="six columns main-col">
-              <h2>{project.subtitle}</h2>
-              <h1>{project.title}</h1>
-              <p>{project.description}</p>
-              <ul className="technologies">
-                {project.technologies.map((tech) => <li>{tech}</li>)}
-              </ul>
-              <ul className="links">
-                {project.links.map((link) => {
-                  return (
-                    <li className={link.className}>
-                      <a href={link.url}>
-                        <span>{link.text}</span>
-                        <i class="fa fa-angle-right"></i>
-                      </a>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-            <div className="six columns image-col">
-                <img src={"images/portfolio/" + project.image} />
+          <div className="project">
+            <div className="row">
+              <div className="six columns main-col">
+                <h2>{project.subtitle}</h2>
+                <h1>{project.title}</h1>
+                <p>{project.description}</p>
+                <ul className="technologies">
+                  {project.technologies.map((tech) => <li>{tech}</li>)}
+                </ul>
+                <ul className="links">
+                  {project.links.map((link) => {
+                    return (
+                      <li className={link.className}>
+                        <a href={link.url}>
+                          <span>{link.text}</span>
+                          <i class="fa fa-angle-right"></i>
+                        </a>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+              <div className="six columns image-col">
+                  <img src={"images/portfolio/" + project.image} />
+              </div>
             </div>
           </div>
+          
         </Fade>
       );
     });

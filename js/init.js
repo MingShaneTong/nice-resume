e411bb74ea8522d5242243bc3cbd4699b9384782
Ming Shane Tong <mstong800@gmail.com>
2023-03-23 21:32:11 +1300

    Updates

--- js/init.js
+++ js/init.js
@@ -1,40 +1,7 @@
-const resizeAction = function() {
-  $("header#home").css({ height: $(window).height() });
-  $("body").css({ width: $(window).width() });
-  if ($(window).width() >= 767) {
-    $("#nav-wrap > .mobile-btn").hide();
-    $("#nav-wrap > ul#nav").show();
-  } else {
-    $("#nav-wrap > .mobile-btn").show();
-    $("#nav-wrap > ul#nav").hide();
-  }
-}
-
 jQuery(document).ready(function($) {
   var time = 300;
   setTimeout(function() {
-    resizeAction();
-
     $("h1.responsive-headline").fitText(1, { minFontSize: "40px", maxFontSize: "90px" });
-
-    $(".smoothscroll").on("click", function(e) {
-      e.preventDefault();
-      var target = $(this).data('anchor'),
-        $target = $(target);
-
-      $("html, body")
-        .stop()
-        .animate(
-          {
-            scrollTop: $target.offset().top
-          },
-          800,
-          "swing"
-        );
-    });
-
-    $("header#home").css({ height: $(window).height() });
-    $(window).on("resize", resizeAction);
 
     $(window).on("scroll", function() {
       var h = $("header#home").height();
